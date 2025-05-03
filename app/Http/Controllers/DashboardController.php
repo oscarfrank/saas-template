16c33ed7852db9b21f4b612206a641054b43025f
oscarfrank <oscarifyfrank@gmail.com>
2025-05-03 18:28:19 +0100

    Added Scaffold for Dashboards

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Storage;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return Inertia::render('dashboard/dashboard');
+    }
+
+    public function adminDashboard(Request $request)
+    {
+        return Inertia::render('dashboard/admin-dashboard');
+    }
+
+    public function lenderDashboard(Request $request)
+    {
+        return Inertia::render('dashboard/lender-dashboard');
+    }
+
+    public function borrowerDashboard(Request $request)
+    {
+        return Inertia::render('dashboard/borrower-dashboard');
+    }
+    
+    
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,22 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    // Dashboard routes
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('dashboard/admin', [DashboardController::class, 'adminDashboard'])->name('admin-dashboard');
+    Route::get('dashboard/lender', [DashboardController::class, 'lenderDashboard'])->name('lender-dashboard');
+    Route::get('dashboard/borrower', [DashboardController::class, 'borrowerDashboard'])->name('borrower-dashboard');
 
     // Products routes
     Route::resource('products', ProductController::class);

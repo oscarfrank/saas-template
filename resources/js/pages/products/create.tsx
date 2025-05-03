727213a7651c07d3669e4d135a50f730cd2c7bb3
oscarfrank <oscarifyfrank@gmail.com>
2025-05-03 16:13:45 +0100

    updated some UI issues

--- resources/js/pages/products/create.tsx
+++ resources/js/pages/products/create.tsx
@@ -1,9 +1,8 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
-import { EditForm } from './components/EditForm';
+import { EditForm } from './components/edit-form';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {

38fa6f81768b6a0923c4ccac05e562a281b354bc
oscarfrank <oscarifyfrank@gmail.com>
2025-05-04 15:41:26 +0100

    Added Roles UI - 100% working

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class Role extends SpatieRole
+{
+    protected $fillable = [
+        'name',
+        'guard_name'
+    ];
+
+    /**
+     * The permissions that belong to the role.
+     */
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_has_permissions');
+    }
+}

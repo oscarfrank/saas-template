83bf4c8354d24e149bca7695cf1179bd1dc536b0
oscarfrank <oscarifyfrank@gmail.com>
2025-06-01 03:33:32 +0100

    Added Open AI Scaffold.

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,36 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Responses\Completions\CreateResponse;
+
 use App\Traits\LevelBasedAuthorization;
 use App\Helpers\AccessLevel;
 
 // Cron job route for checking completed loans
 Route::get('/cron/check-completed-loans', [App\Http\Controllers\CronController::class, 'checkCompletedLoans'])
     ->name('cron.check-completed-loans');
+
+Route::get('ai', function() {
+
+
+    $response = OpenAI::chat()->create([
+        'model' => 'gpt-4o',
+        'messages' => [
+            [
+                'role' => 'user',
+                'content' => 'Who is Oscarmini ',
+            ],
+        ],
+    ]);
+
+
+    return $response->choices[0]->message->content;
+
+
+    
+})->name('ai');
 
 Route::middleware(['auth', 'verified'])->group(function () {
 

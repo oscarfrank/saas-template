8cee2fafb832b1bf276f659c341a2400a929d3d7
oscarfrank <oscarifyfrank@gmail.com>
2026-02-23 01:52:04 +0100

    Make Telescope conditional for local not production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,12 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
 ];
+
+if (env('APP_ENV') === 'local') {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
